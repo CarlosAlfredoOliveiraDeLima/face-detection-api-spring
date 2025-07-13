0fe38593592be6a7c7b4b865d6590820d54a5106
CarlosAlfredoOliveiraDeLima <carlos.alfredo.oliveira.de.lima@gmail.com>
2025-07-12 21:17:43 -0300

    Initialize Spring Boot project for face-detection-api.

---
+++ face-detection-api/src/main/java/com/project/portfolio/facedetectionapi/FaceDetectionApiApplication.java
@@ -0,0 +1,13 @@
+package com.project.portfolio.facedetectionapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FaceDetectionApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FaceDetectionApiApplication.class, args);
+    }
+
+}
---
+++ face-detection-api/src/test/java/com/project/portfolio/facedetectionapi/FaceDetectionApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.project.portfolio.facedetectionapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FaceDetectionApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
